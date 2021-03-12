51cd01bac8a88468110bccbb048cfccef596f0ad
Miszo <kol478@gmail.com>
2021-03-12 22:27:11 +0100

    Change the default debug flag

--- GeoApi/settings.py
+++ GeoApi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ["CV_DEBUG"] if "CV_DEBUG" in os.environ else False
 
 ALLOWED_HOSTS = ['*']
 
