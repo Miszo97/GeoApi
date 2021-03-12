b1cb96b6fcc805a4f5b98348fb6cebae3332654d
Miszo <kol478@gmail.com>
2021-03-12 22:25:48 +0100

    Create IPGeoData model

--- geo_api/models.py
+++ geo_api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class IPGeoData(models.Model):
+    ip = models.CharField(max_length=200)
+    type = models.CharField(max_length=200)
+    continent_code = models.CharField(max_length=200)
+    continent_name = models.CharField(max_length=200)
+    country_code = models.CharField(max_length=200)
+    country_name = models.CharField(max_length=200)
+    region_code = models.CharField(max_length=200)
+    region_name = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    zip = models.CharField(max_length=200)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
