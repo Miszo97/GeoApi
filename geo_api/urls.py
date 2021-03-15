88f6daa8cd72c03fcb2623a7d98be88e0099cc23
Miszo <kol478@gmail.com>
2021-03-15 18:41:39 +0100

    Add a new routing

--- geo_api/urls.py
+++ geo_api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import GeoData
+from .views import GeoData, GeoDataDetail
 
 app_name = 'geo_api'
 
 urlpatterns = [
-    path('', GeoData.as_view(), name='geo_data')
+    path('', GeoData.as_view(), name='geo_data'),
+    path('/<address>', GeoDataDetail.as_view(), name='geo_data_detail'),
 ]
